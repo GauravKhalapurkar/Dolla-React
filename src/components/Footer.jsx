8bd68848f9290028727d37c88e83c2bc2751ca6a
Gaurav K <gaurav.khalapurkar@gmail.com>
2022-07-02 20:58:45 +0530

    footer complete. Website complete.

---
+++ src/components/Footer.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full bg-black text-white text-center">
+      <div className="grid grid-cols-2 lg:grid-cols-5 py-16">
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold border-b-4 border-[color:var(--primary-blue)] inline-block">
+            Products
+          </h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token List</li>
+            <li>Dolla</li>
+          </ul>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold border-b-4 border-[color:var(--primary-blue)] inline-block">
+            Products
+          </h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token List</li>
+            <li>Dolla</li>
+          </ul>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold border-b-4 border-[color:var(--primary-blue)] inline-block">
+            Products
+          </h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token List</li>
+            <li>Dolla</li>
+          </ul>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold border-b-4 border-[color:var(--primary-blue)] inline-block">
+            Products
+          </h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token List</li>
+            <li>Dolla</li>
+          </ul>
+        </div>
+        <div className="mt-4">
+          <h3 className="text-2xl font-bold border-b-4 border-[color:var(--primary-blue)] inline-block">
+            Products
+          </h3>
+          <ul>
+            <li>App</li>
+            <li>Analytics</li>
+            <li>Token List</li>
+            <li>Dolla</li>
+          </ul>
+        </div>
+      </div>
+      <div className="pb-8">
+        All Rights Reserved.{" "}
+        <span className="text-[color:var(--primary-blue)]">Dolla</span>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
